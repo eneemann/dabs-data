5a9068a5fdc3edde1d9a571539f6c59de4f8d532
Erik Neemann <eneemann@utah.gov>
2022-09-29 11:55:41 -0600

    add: zone assignment function

--- dabs_license_calculations.py
+++ dabs_license_calculations.py
@@ -15,8 +15,22 @@ readable_start = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 print("The script start time is {}".format(readable_start))
 today = time.strftime("%Y%m%d")
 
+#: Create variables
 dabs_db = r"C:\Users\eneemann\Documents\ArcGIS\Projects\DABC\DABS_latest_data.gdb"
 dabs_licenses = os.path.join(dabs_db, "DABS_All_Licenses")
+zone_path = os.path.join(dabs_db, 'DABS_Compliance_Zones')
+
+#: Set up polygon assignment fields
+zone_field = 'Zone_ID'
+group_field = 'Group_Name'
+
+#: Create polygon assignment dictionary where key is name of field that needs updated in points layer
+#: format is:
+        #: 'pt_field_name': {'poly_path': path, 'poly_field': field}
+poly_dict = {
+        'Comp_Zone': {'poly_path': zone_path, 'poly_field': zone_field},
+        'Comp_Group': {'poly_path': zone_path, 'poly_field': group_field}
+        }
 
 #: Create dictionaries for attribute look-ups based on two-letter license type code
 dabs_descr = {
@@ -171,6 +185,56 @@ dabs_comp_needed = {
     'TV': 'yes'
 }
 
+#: Create function for compliance zone assignments
+def assign_poly_attr(pts, polygonDict):
+    
+    arcpy.env.workspace = os.path.dirname(pts)
+    arcpy.env.overwriteOutput = True
+    
+    for lyr in polygonDict:
+        # set path to polygon layer
+        polyFC = polygonDict[lyr]['poly_path']
+        print (f'working on {polyFC} ... ')
+        
+        # generate near table for each polygon layer
+        neartable = 'in_memory\\near_table'
+        arcpy.analysis.GenerateNearTable(pts, polyFC, neartable, '1 Meters', 'NO_LOCATION', 'NO_ANGLE', 'CLOSEST')
+        
+        # create dictionaries to store data
+        pt_poly_link = {}       # dictionary to link points and polygons by OIDs 
+        poly_OID_field = {}     # dictionary to store polygon NEAR_FID as key, polygon field as value
+    
+        # loop through near table, store point IN_FID (key) and polygon NEAR_FID (value) in dictionary (links two features)
+        with arcpy.da.SearchCursor(neartable, ['IN_FID', 'NEAR_FID', 'NEAR_DIST']) as nearCur:
+            for row in nearCur:
+                pt_poly_link[row[0]] = row[1]       # IN_FID will return NEAR_FID
+                # add all polygon OIDs as key in dictionary
+                poly_OID_field.setdefault(row[1])
+        
+        # loop through polygon layer, if NEAR_FID key in poly_OID_field, set value to poly field name
+        with arcpy.da.SearchCursor(polyFC, ['OID@', polygonDict[lyr]['poly_field']]) as polyCur:
+            for row in polyCur:
+                if row[0] in poly_OID_field:
+                    poly_OID_field[row[0]] = row[1]
+        
+        # loop through points layer, using only OID and field to be updated
+        with arcpy.da.UpdateCursor(pts, ['OID@', lyr]) as uCur:
+            for urow in uCur:
+                try:
+                    # search for corresponding polygon OID in polygon dictionay (polyDict)
+                    if pt_poly_link[urow[0]] in poly_OID_field:
+                        # if found, set point field equal to polygon field
+                        # IN_FID in pt_poly_link returns NEAR_FID, which is key in poly_OID_field that returns value of polygon field
+                        urow[1] =  poly_OID_field[pt_poly_link[urow[0]]]
+                except:         # if error raised, just put a blank in the field
+                    urow[1] = ''
+                uCur.updateRow(urow)
+    
+        # Delete in memory near table
+        arcpy.management.Delete(neartable)
+
+
+
 #: Calculate DABS fields from License Number
 update_count = 0
 #:               0          1             2            3            4              5
@@ -195,8 +259,11 @@ lon_calc = 'arcpy.PointGeometry(!Shape!.centroid, !Shape!.spatialReference).proj
 arcpy.management.CalculateField(dabs_licenses, 'Point_Y', lat_calc, "PYTHON3")
 arcpy.management.CalculateField(dabs_licenses, 'Point_X', lon_calc, "PYTHON3")
 
+#: Call polygon assignment function
+assign_poly_attr(dabs_licenses, poly_dict)
+
 #: Stop timer and print end time in UTC
 print("Script shutting down ...")
 readable_end = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 print("The script end time is {}".format(readable_end))
-print("Time elapsed: {:.2f}s".format(time.time() - start_time))dabs_licenses
+print("Time elapsed: {:.2f}s".format(time.time() - start_time))
