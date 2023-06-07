911ff7074ee489474dfe4317b8927b0b409d13b9
Erik Neemann <eneemann@utah.gov>
2023-06-07 17:05:59 -0600

    add: credentials_template

---
+++ credentials_template.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+credentials.py to hold connection information
+
+"""
+
+AGOL_USER = '' #: insert AGOL username
+AGOL_PASSWORD = ''  #: insert AGOL password
+AGOL_LAYER = r'' #: insert AGOL layer URL (example format: https://services1.arcgis.com/{X}/arcgis/rest/services/{x}/FeatureServer/{x})
+SERVICE_ACCOUNT_JSON = r'' #: insert path to local service account JSON for Google Sheets
+SHEET_ID = '' #: insert Google Sheet ID for where updates are posted
